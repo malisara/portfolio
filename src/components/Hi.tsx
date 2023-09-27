ccf239fa2af9cf5ef0c47e36722bf124940652e7
Sara Mali <mali.sara7@gmail.com>
2023-09-27 20:00:23 +0200

    animation

--- src/components/Hi.tsx
+++ src/components/Hi.tsx
@@ -1,7 +1,39 @@
-type Props = {};
+import LottieGif from "../lotties/codingGirlGif.json";
+import Lottie from "lottie-react";
 
-function Hi({}: Props) {
-  return <div>Hi</div>;
+function Hi(): JSX.Element {
+  return (
+    <div
+      className="flex flex-col lg:flex-row lg:justify-between
+      mt-[5rem] md:mt-[7rem]"
+    >
+      <div className="order-2 lg:order-1 lg:text-start xl:ps-[10%]">
+        <h1
+          className="text-center mt-6 lg:mt-[3rem] 2xl:mt-[5rem] 
+          lg:text-start text-4xl lg:text-6xl"
+        >
+          Hi, my name is
+          <span className="font-extrabold"> Sara.</span>
+        </h1>
+
+        <p className="mt-[3rem] text-lg md:text-2xl leading-10">
+          I am a self-taught{" "}
+          <span className="font-extrabold">web developer </span>
+          with an <span className="font-bold">MBA</span> degree.
+        </p>
+      </div>
+      <div className="order-1 lg:order-2 xl:pe-[10rem]">
+        <Lottie
+          animationData={LottieGif}
+          height={50}
+          width={50}
+          autoplay={false}
+          loop={false}
+          // style={{ height: 500 }}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Hi;
