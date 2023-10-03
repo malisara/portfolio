109abdf9b3cd022229f721a9350c2e36dbe88aef
Sara Mali <mali.sara7@gmail.com>
2023-10-03 18:13:15 +0200

    title update

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,10 +1,11 @@
 type Props = { text: string };
 
-function Title({ text }: Props) {
+function Title({ text }: Props): JSX.Element {
   return (
     <div
-      className="mt-[6rem] mb-[2rem] text-3xl
+      className="mt-[6rem] md:mt-[10rem] mb-[2rem] md:mb-[4rem] text-3xl
       lg:text-4xl font-bold text-center"
+      id={text.toLowerCase()}
     >
       {text}
     </div>
