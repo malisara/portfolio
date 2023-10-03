1d2915ea9423ef1cc5fc162a15c86d0948e8ddfb
Sara Mali <mali.sara7@gmail.com>
2023-10-03 18:07:18 +0200

    navbar update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,12 @@
 //todo change bg color
-//add links
-
-import { useRef, useEffect, useState } from "react";
 
 import { AnimatePresence, motion } from "framer-motion";
+import useMediaQuery from "../hooks/useMediaQuery";
+import { useRef, useEffect, useState } from "react";
 import { AiOutlineClose } from "react-icons/ai";
 import { PiTriangleDuotone } from "react-icons/pi";
 import { RxHamburgerMenu } from "react-icons/rx";
-
-import useMediaQuery from "../hooks/useMediaQuery";
+import { Link, animateScroll } from "react-scroll";
 
 function Navbar(): JSX.Element | null {
   const isDesktop = useMediaQuery("(min-width: 1060px)");
@@ -17,6 +15,11 @@ function Navbar(): JSX.Element | null {
   const ref = useRef<HTMLDivElement>(null);
 
   const navbarBg = navOnTop ? "bg-transparent" : "bg-stone-50 border-b-2";
+  const navLinks = [
+    { label: "Projects" },
+    { label: "Technologies" },
+    { label: "Contact" },
+  ];
 
   useEffect(() => {
     const handleScroll = () => {
@@ -46,44 +49,70 @@ function Navbar(): JSX.Element | null {
         px-10 z-10 transtion-all duration-300 ${navbarBg}`}
         ref={ref}
       >
-        <PiTriangleDuotone className="text-2xl" />
+        <PiTriangleDuotone
+          className="text-2xl cursor-pointer"
+          onClick={() =>
+            animateScroll.scrollToTop({ duration: 500, smooth: true })
+          }
+        />
 
-        {isDesktop ? (
-          <div className="flex gap-5 font-bold text-lg ps-5">
-            <p>Project</p>
-            <p>Technologies</p>
-            <p>Contact</p>
-          </div>
-        ) : (
-          <div className="ml-auto">
+        <div className={`flex gap-5 text-lg ${isDesktop ? "ps-5" : "ml-auto"}`}>
+          {isDesktop ? (
+            navLinks.map((link, i) => (
+              <Link
+                key={i}
+                to={link.label.toLowerCase()}
+                smooth={true}
+                duration={500}
+                className="cursor-pointer"
+                offset={-100}
+              >
+                {link.label}
+              </Link>
+            ))
+          ) : (
             <button onClick={() => setMobileNavOpen(!mobileNavOpen)}>
               <RxHamburgerMenu className="text-lg" />
             </button>
-          </div>
-        )}
+          )}
+        </div>
       </div>
 
       <AnimatePresence>
         {!isDesktop && mobileNavOpen && (
           <motion.div
             className="fixed top-0 w-[50vw] h-[100vh] z-20
-          bg-stone-50 drop-shadow-lg ps-10"
+            bg-stone-50 drop-shadow-lg ps-10"
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
             transition={{ duration: 0.3 }}
           >
             <div className="pe-10 flex pt-5 pb-10 justify-between items-center">
-              <PiTriangleDuotone className="text-2xl" />
+              <PiTriangleDuotone
+                className="text-2xl cursor-pointer"
+                onClick={() =>
+                  animateScroll.scrollToTop({ duration: 500, smooth: true })
+                }
+              />
               <button onClick={() => setMobileNavOpen(!mobileNavOpen)}>
                 <AiOutlineClose />
               </button>
             </div>
 
             <div className="flex flex-col gap-10">
-              <p>Project</p>
-              <p>Technologies</p>
-              <p>Contact</p>
+              {navLinks.map((link, i) => (
+                <Link
+                  key={i}
+                  to={link.label.toLowerCase()}
+                  smooth={true}
+                  duration={500}
+                  className="cursor-pointer"
+                  offset={-100}
+                >
+                  {link.label}
+                </Link>
+              ))}
             </div>
           </motion.div>
         )}
